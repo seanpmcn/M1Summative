664eb45db681a9d0eb6e12283ecfe568b1eca023
seanpmcn <46979553+seanpmcn@users.noreply.github.com>
2021-08-09 15:38:51 -0400

    Add Hello.java program

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**
+ *@author Sean McNally
+ *email: seanmcnally@outlook.com
+ *date: 2021 08 09
+ *purpose: My first Hello World program in NetBeans
+ */
+
+//source-packages
+package sourcepackage.week1test;
+
+/**
+ *
+ * @author SeanM
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Sean McNally");
+    }
+}
